68ee30bdea7146048d422eba2207cbb18baac8f1
Andrew <awebb@ultralinq.com>
2022-01-12 18:09:19 -0500

    Set up fetch function getTwelveRandomUsers

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log(`you're all hooked up`)
+
+// Fetch info from api
+async function getTwelveRandomUsers() {
+    const response = await fetch(`https://randomuser.me/api/?nat=us&results=12`)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            return data
+        })
+
+    return response.results
+}
+
+
+async function main() {
+    const users = await getTwelveRandomUsers()
+
+    // NEXT: Simplify and flatten the users ( and maybe add keys for the missing values ).
+
+
+}
+
+main()
+
